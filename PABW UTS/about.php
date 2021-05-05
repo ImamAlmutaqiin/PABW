34bd1d098d262a92f563adce0347a4fc081250df
ImamAlmutaqiin <56435310+ImamAlmutaqiin@users.noreply.github.com>
2021-05-06 06:06:28 +0700

    Add files via upload

---
+++ PABW UTS/about.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet">
+  
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-warning">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Berita</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarScroll">
+      <ul class="navbar-nav ml-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+       
+
+<section class="my-5">
+  <div class="py-3">
+    <h2 class="text-center">BERITA</h2>
+  </div>
+  <div class="container-fluid">
+  <div class="row">
+      <div class="col-lg-6 col-md-6 col-12">
+      <a href="about.php" class="btn btn-success">Baca selengkapnya</a>
+    </div>
+  </div>
+</section>
+
+
+<footer>
+  <p class="p-2 bg-warning text-white text-center">Kabar.com</p>
+</footer>
+
+</body>
+</html>
